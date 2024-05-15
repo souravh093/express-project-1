cace24a6d6131677c27b407081f6922986b8331e
Sourave halder <souravh093@gmail.com>
2024-05-15 15:38:47 +0600

    fix the problem in the assignement

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,79 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
 
 // middleware
-app.use(express.json())
-app.use(express.text())
+app.use(express.json());
+app.use(express.text());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json(
-    {
-        message: "Server is running..."
-    }
-  );
+// router
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/courses", courseRouter);
+
+userRouter.post("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+
+  res.json({
+    success: true,
+    message: "User is created successfully",
+    data: user,
+  });
 });
 
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
 
-app.post("/", (req: Request, res: Response) => {
-    console.log(req.body);
-    res.json(
-        {
-            message: "Api touch"
-        }
-    )
-})
+  res.json({
+    success: true,
+    message: "Course is created successfully",
+    data: course,
+  });
+});
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.body);
+  next();
+};
+
+app.get(
+  "/",
+  logger,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.send(someting);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
+app.post("/", logger, (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  res.json({
+    message: "Api touch",
+  });
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(400).json({
+    success: false,
+    message: "Not Found",
+  });
+});
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+});
 
 export default app;
